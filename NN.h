3043419528036030c9efe095b027d1d393e7ac44
zcc199710 <34901898+zcc199710@users.noreply.github.com>
2018-01-03 15:09:02 +0800

    Add files via upload

--- NN.h
+++ NN.h
@@ -17,32 +17,38 @@ public:
 	//初始化权值矩阵，调用initWeight()函数.
 	void BeginWeights(int type = 0, double a = 0., double b = 0.1);
 	//初始化偏置项.
-	void Begindross(cv::Scalar& bias);
+	void Begindross(Scalar& bias);
+	//sigmoid函数
+	Mat sigmoid(Mat &x);
+	//微分 函数 （Derivative） 简称求导
+	Mat derivativeFunction(Mat& fx, string func_type);
+	//计算输出误差和目标函数，所有输出误差平方和的均值作为需要最小化的目标函数（在反向传播中运用）
+	void calcLoss(Mat &output, Mat &target, Mat &output_error, float &loss);
 	//forward()：执行前向运算，包括线性运算和非线性激活，同时计算误差
 	void Forward_Trans();   //同时激活函数包含在function里
 	//backward()：执行反向传播，调用updateWeights()函数更新权值。
 	void Backward_Trans();
 	//从xml文件中从指定的列开始提取一定数目的样本和标签。默认从第0列开始读取 
-	void get_file_from_xml(std::string filename, cv::Mat& input, cv::Mat& label, int sample_num, int start = 0);
+	void get_file_from_xml(string filename, Mat& input, Mat& label, int sample_num, int start = 0);
 	//Train  训练函数（小于设定的阙值 作为迭代终止条件）
-	void train(cv::Mat input, cv::Mat target_, float loss_threshold, bool draw_loss_curve = false);
+	void train(Mat input, Mat target_, float loss_threshold, bool draw_loss_curve = false);
 	//经过train 之后 就得到了一个超级矩阵（权值矩阵模型）
 	//Test  测试函数  测试没有训练过的数据 看看正确率为多少  其中调用了下面的predite_one函数 远离还是一样的（代替了一次前向传播）
-	void test(cv::Mat &input, cv::Mat &target_);
+	void test(Mat &input, Mat &target_);
 	//Predict  仅仅一个样例  与上面的test照应
-	int forward(cv::Mat &input);
+	int forward(Mat &input);
 	//Predict  多余一个的样例 循环调用 predict_one()
-	vector<int> predict(cv::Mat &input);
+	vector<int> predict(Mat &input);
 	//训练过一次之后要把模型给保存起来，要不然下次还要训练
-	void save_model(std::string filename);
+	void save_model(string filename);
 	//加载模型
-	void load_model(std::string filename);
+	void load_model(string filename);
 
 protected:
 	//初始化矩阵
-	void BeginWeight(cv::Mat &dst, int type, double a, double b);
+	void BeginWeight(Mat &dst, int type, double a, double b);
 	//激活函数
-	cv::Mat activationFunction(cv::Mat &x, std::string func_type);
+	Mat activationFunction(Mat &x, string func_type);
 	//计算delta误差
 	void deltaError();
 	//更新权重
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "NN.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+	//设置神经网络每一层的神经元数目   三层（784，100，10）给定数据集的固定格式
+	vector<int> Layer_Neurons_nums = { 784,100,10 };
+
+	// 神经网络结构  权重 偏执 
+	NN nn;
+	nn.BeginNN(Layer_Neurons_nums);
+	nn.BeginWeights(0, 0., 0.01);
+	nn.Begindross(Scalar(0.05));
+
+	//获取训练和测试数据 
+	Mat input, label, test_input, test_label;
+	int sample_number = 200;
+	nn.get_file_from_xml("data/input_label.xml", input, label, sample_number);
+	nn.get_file_from_xml("data/input_label.xml", test_input, test_label, 200, 800);
+
+	float loss_threshold = 0.5;
+	nn.rates = 0.3;
+	nn.output_Neurons = 2;
+	nn.Activation_Functions = "sigmoid";
+
+	nn.train(input, label, loss_threshold, true);
+	nn.test(test_input, test_label);
+
+	//保存模型 
+	nn.save_model("models/model_sigmoid.xml");
+
+	getchar();
+	return 0;
+}
